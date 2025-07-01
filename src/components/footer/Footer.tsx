308f6f7959453d7a881d430bf06c4f69543d2b68
MukitHossen7 <hossenmukit7@gmail.com>
2025-07-02 00:57:30 +0600

    Refactor Footer and MainLayout components to improve structure and add navigation links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,48 @@
+import { Link } from "react-router";
+
 const Footer = () => {
   return (
-    <div>
-      <h1>This is Footer</h1>
+    <div className="w-11/12 md:w-11/12 lg:w-11/12 xl:container mx-auto py-10">
+      <div className="flex flex-col items-center text-center">
+        {/* Logo */}
+        <div className="flex flex-col items-center">
+          <Link to="/" className="inline-flex items-center mb-2">
+            <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+              Library Management
+            </span>
+          </Link>
+        </div>
+
+        {/* Links Section */}
+
+        <div className="flex justify-center text-sm text-gray-600 py-5">
+          {/* Book Navigation */}
+          <div>
+            <ul className=" flex items-center gap-6">
+              <li>
+                <Link to="/books" className="hover:text-purple-600">
+                  All Books
+                </Link>
+              </li>
+              <li>
+                <Link to="/add-book" className="hover:text-purple-600">
+                  Add Book
+                </Link>
+              </li>
+              <li>
+                <Link to="/borrow-summary" className="hover:text-purple-600">
+                  Borrow Summary
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* Footer Bottom */}
+        <div className="w-full border-t pt-5 text-sm text-gray-600 text-center">
+          © Copyright 2020 Lorem Inc. All rights reserved.
+        </div>
+      </div>
     </div>
   );
 };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,10 @@ const MainLayout = () => {
   return (
     <div>
       <Navbar></Navbar>
-      <Outlet></Outlet>
+      <div className="min-h-[calc(100vh-304px)]">
+        <Outlet></Outlet>
+      </div>
+
       <Footer></Footer>
     </div>
   );
